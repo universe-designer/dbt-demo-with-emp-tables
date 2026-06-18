49ca7a903c60e00cc01c50b965b47e61fb580131
Daniel Fritzler <118835568+universe-designer@users.noreply.github.com>
2026-06-18 18:16:02 +0200

    Update fact_anstellung.sql

--- models/mart/fact_anstellung.sql
+++ models/mart/fact_anstellung.sql
@@ -2,16 +2,17 @@ with
 
 source as (
 
-    select * from {{ source('scott', 'emp') }}
+    select * from {{ ref('stg_scott__emp') }}
+
 
 ),
 
 renamed as (
 
     select 
-    empno AS dim_mitarbeiter_nr, 
-    deptno AS dim_abteilung_nr,
-    hiredate AS dim_zeit,
+    mitarbeiter_nr AS dim_mitarbeiter_nr, 
+    abteilung_nr AS dim_abteilung_nr,
+    einstellung_datum AS dim_zeit,
     1 AS ms_anstellung
 
     from source
